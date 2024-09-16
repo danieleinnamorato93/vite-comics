c6a9cae11f18fa0bc3bb3ccedbea269af89753a5
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-16 17:21:49 +0200

    Header done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,16 +3,16 @@ export default {
   data() {
     return {
       navLinks :[
-        "Characters",
-        "Comics",
-        "Movies",
-        "Tv",
-        "Games",
-        "Collectibles",
-        "Videos",
-        "Fans",
-        "News",
-        "Shop",
+        "CHARACTERS",
+        "COMICS",
+        "MOVIES",
+        "TV",
+        "GAMES",
+        "COLLECTIBLES",
+        "VIDEOS",
+        "FANS",
+        "NEWS",
+        "SHOP",
     ]
     }
       
@@ -24,7 +24,9 @@ export default {
 <header>
 <div class="logo"><img src="../assets/img/dc-logo.png" alt="header-logo"></div>
 <ul>
-
+<li v-for="navItem in navLinks">
+ <a href="#">{{ navItem }}</a> 
+</li>
 </ul>
 </header>
 </template>
@@ -34,7 +36,36 @@ header{
     background-color: white;
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
+    .logo >img {
+    height: 60px;
+    width: auto;
+    margin-top: 5px;
+    margin-bottom: 20px;
+  }
+    ul {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+  li {
+    margin-right: 10px;
+    font-size: 12px;
+    font-weight: bold;
+    color: #4A4C4C;
+    a{
+      color: inherit;
+      &:hover {
+    color: #0282F9;
+    border-bottom: 2px solid #0282F9;
+
+    }
+    }
+  
+  }
+
 }
+}
+
 </style>
